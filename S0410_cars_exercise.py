8dc3ea16b4217ea891127e943437a8d4844f7c91
Bejap <benjaminsch08@gmail.com>
2024-04-03 09:27:48 +0200

    prøver igen

---
+++ S0410_cars_exercise.py
@@ -0,0 +1,14 @@
+
+
+def drive_car():
+    print("RRROOOaRR")
+
+Car1_wheels = 4
+Car1_max = 180
+Car2_wheels = 8
+Car2_max = 150
+
+print("Car 2: wheels {0} max speed is {1}".format(Car1_wheels, Car1_max))
+print("Car 2: wheels {0} max speed is {1}".format(Car2_wheels, Car2_max))
+
+drive_car()
