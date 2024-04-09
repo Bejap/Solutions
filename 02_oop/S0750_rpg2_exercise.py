a1a258146cc7711628c8a8551ba328e16ec0349c
Bejap <benjaminsch08@gmail.com>
2024-04-09 10:53:42 +0200

    EXIT HERE

--- 02_oop/S0750_rpg2_exercise.py
+++ 02_oop/S0750_rpg2_exercise.py
@@ -4,33 +4,20 @@ Som altid skal du læse hele øvelsesbeskrivelsen omhyggeligt, før du begynder 
 
 Byg videre på din løsning af afsnit 1.
 
-Del 1:
-    Opfind to nye klasser, som arver fra klassen Character. For eksempel Hunter og Magician.
-    Dine nye klasser skal have deres egne ekstra metoder og/eller attributter.
-    Måske overskriver de også metoder eller attributter fra klassen Character.
-
-Del 2:
-    Lad i hovedprogrammet objekter af dine nye klasser (dvs. rollespilfigurer) kæmpe mod hinanden,
-    indtil den ene figur er død. Udskriv, hvad der sker under kampen.
-
 I hver omgang bruger en figur en af sine evner (metoder). Derefter er det den anden figurs tur.
 Det er op til dig, hvordan dit program i hver tur beslutter, hvilken evne der skal bruges.
 Beslutningen kan f.eks. være baseret på tilfældighed eller på en smart strategi
 
-Del 3:
-    Hver gang en figur bruger en af sine evner, skal du tilføje noget tilfældighed til den anvendte evne.
 
-Del 4:
-    Lad dine figurer kæmpe mod hinanden 100 gange.
-    Hold styr på resultaterne.
-    Prøv at afbalancere dine figurers evner på en sådan måde, at hver figur vinder ca. halvdelen af kampene.
+
+
 
 Hvis du går i stå, kan du spørge google, de andre elever eller læreren (i denne rækkefølge).
 
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> done
 Fortsæt derefter med den næste fil."""
-
+import random
 class Character:
 
     def __init__(self, name, max_health, attackpower):
@@ -39,43 +26,41 @@ class Character:
         self.attackpower = attackpower
         self._current_health = self.max_health
 
-
-
     def __repr__(self):
-        return f'''{self.name} has {self.max_health} and an attackpower of {self.attackpower}.
-        Current health is {self._current_health}'''
+        return f'''{self.name} the {self.__class__.__name__} has {self.max_health} health and an attackpower of {self.attackpower}.
+        Current health is {round(self._current_health, 2)}\n'''
 
-
+    # Del 3:
+    #     Hver gang en figur bruger en af sine evner, skal du tilføje noget tilfældighed til den anvendte evne.
 
     def hit(self, other):
-        if type(other) == Horseman:
-            other.get_hit(self.attackpower)
-        else:
-            other._current_health -= self.attackpower
-            print("The hit was successfull, and the hit took the opponents health to ", other._current_health, "\n")
+        if type(other):
+            other.get_hit(round(self.attackpower * (random.uniform(0.2, 2)), 2))
 
     def get_healed(self, healpower):
-        self._current_health += healpower
+        self._current_health += (round(healpower * random.uniform(0.2, 2), 2))
 
     def get_hit(self, opponentdmg):
         self._current_health -= opponentdmg
+        print(f"hit was successfull and took the health of {self.name} to \n\t {round(self._current_health,2)}\n")
 
     def dead(self):
         if self._current_health <= 0:
+            print(f'{self.name} has died')
             return True
-        else:
-            return False
+
+    def health(self):
+        return self._current_health
 
 
 
 class Healer(Character):
     def __init__(self, name, max_health, healpower):
+        super().__init__(name, max_health, attackpower=0)
         self.name = name
         self.max_health = max_health
         self.attackpower = 0
         self.healpower = healpower
-        self._current_health = self.max_health
-
 
     def __repr__(self):
         info = super().__repr__()
@@ -84,92 +69,101 @@ class Healer(Character):
     def heal(self, other):
         other.get_healed(self.healpower)
 
-    def get_healed(self, healpower):
-        self._current_health += healpower
-
-    def get_hit(self, opponentdmg):
-        self._current_health -= opponentdmg
-
+# Del 1:
+#     Opfind to nye klasser, som arver fra klassen Character. For eksempel Hunter og Magician.
+#     Dine nye klasser skal have deres egne ekstra metoder og/eller attributter.
+#     Måske overskriver de også metoder eller attributter fra klassen Character.
 
 class Magician(Character):
     def __init__(self, name, max_health, healpower, attackpower, magicpower):
+        super().__init__(name, max_health, attackpower)
         self.name = name
         self.max_health = max_health
         self.healpower = healpower
         self.attackpower = attackpower
         self.magicpower = magicpower
-        self._current_health = self.max_health
 
     def __repr__(self):
         info = super().__repr__()
-        return info + f'''\n       The magician has {self.magicpower} and {self.healpower} healpower\n'''
+        return info + f'''\n       The magician has {self.magicpower} magicpower and {self.healpower} healpower\n'''
 
     def heal(self, other):
         other.get_healed(self.healpower)
 
-
     def hit(self, other):
         other.get_hit(self.attackpower)
-
-    def get_healed(self, healpower):
-        self._current_health += healpower
-
-    def get_hit(self, opponentdmg):
-        self._current_health -= opponentdmg
-
 
     def meditate(self):
         self.magicpower += 10 + (0.1 * self.magicpower)
         self.attackpower += self.magicpower * (0.001 * self.magicpower + 0.01)
         self.healpower += self.magicpower * (0.001 * self.magicpower + 0.01)
-        print("The meditation was successfull, and the Magician now has ", self.magicpower, "magicpower, ", self.healpower, "healpower and ", self.attackpower, "attackpower\n" )
+        print("The meditation was successfull, and the Magician now has ", round(self.magicpower,2), "magicpower, ", round(self.healpower,2),  "healpower and ", round(self.attackpower, 2), "attackpower\n")
 
 class Horseman(Character):
     def __init__(self, name, max_health, attackpower, horsehealth):
+        super().__init__(name, max_health, attackpower)
         self.name = name
         self.max_health = max_health
         self.attackpower = attackpower
         self.horsehealth = horsehealth
-        self._current_health = max_health
         self._current_horse_health = horsehealth
 
     def __repr__(self):
         info = super().__repr__()
-        return info + f'''\n        The Horse has health, {self._current_horse_health}
-        The Horse current health is {self._current_horse_health}'''
+        return info + f'''\n        The Horse max health is {self.horsehealth}
+        The Horse current health is {self._current_horse_health}\n'''
+
     def get_hit(self, opponentdmg):
         if self._current_horse_health <= 0:
             self._current_health -= opponentdmg
-            print("The hit was successfull, and the hit took the horsemans health to ", self._current_health, "\n")
+            print(f"The hit was successfull, and the hit took the {self.name} health to \n\t", self._current_health, "\n")
         else:
             self._current_horse_health -= opponentdmg * 2
-            print("The hit was successfull, and the hit took the horse health to ", self._current_horse_health, "\n")
+            print("The hit was successfull, and the hit took the horse health to \n\t", self._current_horse_health, "\n")
 
     def hit(self, opponent):
         if self._current_horse_health <= 0:
-            opponent.get_hit(self.attackpower)
+            opponent.get_hit(round(self.attackpower * (random.uniform(0.5, 2)), 2))
         else:
-            opponent.get_hit(self.attackpower * 2)
+            opponent.get_hit(round(self.attackpower * 2 * (random.uniform(0.5, 2)), 2))
 
-    def get_healed(self, healpower):
-        self._current_health += healpower
-
-
-t = Character("oprah", 600, 120)
-y = Healer("Tom", 450, 100)
-m = Magician("hom", 1200, 20, 30, 100)
-h = Horseman("ika", 750, 70, 450)
-
-print(t)
-print(y)
-print(m)
-print(h)
+# Del 2:
+#     Lad i hovedprogrammet objekter af dine nye klasser (dvs. rollespilfigurer) kæmpe mod hinanden,
+#     indtil den ene figur er død. Udskriv, hvad der sker under kampen.
 
 
-for i in range(10):
-    if t.dead() or h.dead():
-        break
-    t.hit(h)
-    h.hit(t)
+def fight():
+
+    t = Character("Oprah", 600, 120)
+    y = Healer("Tom", 450, 100)
+    m = Magician("Tarija", 1200, 20, 30, 100)
+    h = Horseman("Ika", 750, 70, 450)
+    while not any([t.dead(), y.dead(), m.dead(), h.dead()]):
+        for attacker in [t, y, m, h]:
+            opponent = random.choice([c for c in [t, m, h] if c != attacker and not c.dead()])
+            attacker.hit(opponent)
+            if opponent.dead():
+                return opponent
+
+def main():
+    twin = ywin = mwin = hwin = 0
+    for i in range(100):
+        winner = fight()
+        if winner is not None:
+            if not Character == type(winner):
+                twin += 1
+            if not Magician == type(winner):
+                mwin += 1
+            if not Horseman == type(winner):
+                hwin += 1
+    print("Character wins: {0}, Magician wins: {1}, Horseman wins: {2}".format(twin, mwin, hwin))
+
+main()
+
+# Del 4:
+#     Lad dine figurer kæmpe mod hinanden 100 gange.
+#     Hold styr på resultaterne.
+#     Prøv at afbalancere dine figurers evner på en sådan måde, at hver figur vinder ca. halvdelen af kampene.
+
 
 
