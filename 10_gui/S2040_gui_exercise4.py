0facb2e1da3b4d21bb857e1ef589cca124611f33
Bejap <benjaminsch08@gmail.com>
2024-05-02 11:30:01 +0200

    .

--- 10_gui/S2040_gui_exercise4.py
+++ 10_gui/S2040_gui_exercise4.py
@@ -1,40 +1,33 @@
-"""Opgave "GUI step 3":
+""" Opgave "GUI step 4":
 
 Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
 
 Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
 
-Bruge det, du har lært i GUI-eksempelfilerne, og byg den GUI, der er afbildet i images/gui_2030.png
+Bruge det, du har lært i GUI-eksempelfilerne, og byg den GUI, der er afbildet i images/gui_2040.png
 
-Genbrug din kode fra "GUI step 2".
+Genbrug din kode fra "GUI step 3".
 
-GUI-strukturen bør være som følger:
-    main window
-        labelframe
-            frame
-                treeview and scrollbar
-            frame
-                labels and entries
-            frame
-                buttons
+Fyld treeview'en med testdata.
+Leg med farveværdierne. Find en farvekombination, som du kan lide.
 
 Funktionalitet:
     Klik på knappen "clear entry boxes" sletter teksten i alle indtastningsfelter (entries).
-
+    Hvis du klikker på en datarække i træoversigten, kopieres dataene i denne række til indtastningsfelterne.
 
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 from tkinter import *
 from tkinter import ttk
-
-
 def main():
     win = Tk()
     win.title('my first GUI')
-    win.geometry('500x500')
+    win.geometry('491x472')
     labeled_frame = LabelFrame(win, text='Container')
     labeled_frame.grid(row=0, column=0, padx=10, pady=5, sticky=N)
+
+    data_list = [("1", "1000", "Oslo"), ("2", "2000", "Chicago"), ("3", "3000", "Milano"), ("4", '4000', 'Amsterdam')]
 
     def empty_entry():
         entry_1.delete(0, END)
@@ -42,9 +35,32 @@ def main():
         entry_3.delete(0, END)
         entry_4.delete(0, END)
 
+    def edit_record(event, tree):
+        index_selected = tree.focus()
+        values = tree.item(index_selected, 'values')
+        entry_1.delete(0, END)
+        entry_2.delete(0, END)
+        entry_3.delete(0, END)
+        entry_1.insert(0, values[0])
+        entry_2.insert(0, values[1])
+        entry_3.insert(0, values[2])
+
+    def the_data(tree):
+        count = 0
+        for record in data_list:
+            if count % 2 == 0:
+                tree.insert(parent='', index='end', text='', values=record, tags='evenrow')
+            else:
+                tree.insert(parent='', index='end', text='', values=record, tags='oddrow')
+            count += 1
+
     treeview_background = "#eeeeee"
     treeview_foreground = "black"
     treeview_selected = "#773333"
+
+    evenrow = "grey81"
+    oddrow = "grey87"
+
     style = ttk.Style()
     style.theme_use('default')
     style.configure("Treeview", background=treeview_background, foreground=treeview_foreground, rowheight=24, fieldbackground=treeview_background)
@@ -61,11 +77,15 @@ def main():
     tree_1.column("col1", width=40, anchor=E)
     tree_1.column("col2", width=80, anchor=W)
     tree_1.column("col3", width=180, anchor=W)
-
     tree_1.heading("col1", text='Id', anchor=CENTER)
     tree_1.heading("col2", text='Weight', anchor=CENTER)
     tree_1.heading("col3", text='Destination', anchor=CENTER)
 
+    tree_1.tag_configure('oddrow', background=oddrow)
+    tree_1.tag_configure('evenrow', background=evenrow)
+    the_data(tree_1)
+
+    tree_1.bind("<ButtonRelease-1>", lambda event: edit_record(event, tree_1))
 
     frame_1 = Frame(labeled_frame)
     frame_1.grid(row=3, column=0, padx=15, pady=15, sticky=N)
@@ -90,7 +110,6 @@ def main():
     up_to_date.grid(row=0, column=1, padx=(10, 0))
     del_for_us = Button(frame_2, text='Delete')
     del_for_us.grid(row=0, column=2, padx=(10, 0))
-
     clearence = Button(frame_2, text='Clear Entry Boxex', command=empty_entry)
     clearence.grid(row=0, column=3, padx=(10, 0))
 
