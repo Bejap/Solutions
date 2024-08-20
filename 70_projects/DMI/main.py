f2910ba1f72a08f09678b6766f57fcd42c9ea98d
Bejap <benjaminsch08@gmail.com>
2024-08-20 15:03:03 +0200

    working on API

--- 70_projects/DMI/main.py
+++ 70_projects/DMI/main.py
@@ -5,7 +5,7 @@ def fetch_data(api_url, api_key, limit):
     offset = 0
     while len(all_data) < limit:
         response = requests.get(
-            f'{api_url}?bbox-crs=https%3A%2F%2Fwww.opengis.net%2Fdef%2Fcrs%2FOGC%2F1.3%2FCRS84&api-key={api_key}&offset={offset}'
+            f'{api_url}?bbox=7,54,16,58&api-key={api_key}&offset={offset}'
         )
         data = response.json()
         features = data.get('features', [])
@@ -15,36 +15,26 @@ def fetch_data(api_url, api_key, limit):
         offset += len(features)
     return all_data
 
-api_url = 'https://dmigw.govcloud.dk/v2/lightningdata/collections/sensordata/items'
+api_url = 'https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items'
 api_key = '1eb30e5a-fd28-4df4-85ad-0d14b2ad81b9'
-record_limit = 2000
+limit = 2000
 
-features = fetch_data(api_url, api_key, record_limit)
+features = fetch_data(api_url, api_key, limit)
 
-# Process features
-coordinates = [feature['geometry']['coordinates'] for feature in features]
+
 created = [feature['properties']['created'] for feature in features]
+u = []
+i = 0
+for list in range(len(created)):
+    i = created[list]
+    listing = created[list]
+    print(listing)
+    var = listing[:4]
+    print(var)
+    j = 0
+    u.append(var)
 
-# Separate the coordinates into longitude (x) and latitude (y)
-longitudes = [coord[0] for coord in coordinates]
-latitudes = [coord[1] for coord in coordinates]
 
-# Find min and max values
-min_longitude = min(longitudes)
-max_longitude = max(longitudes)
-min_latitude = min(latitudes)
-max_latitude = max(latitudes)
+print(u)
+print(len(u))
 
-# Find coordinates corresponding to min and max values
-min_longitude_coord = coordinates[longitudes.index(min_longitude)]
-max_longitude_coord = coordinates[longitudes.index(max_longitude)]
-min_latitude_coord = coordinates[latitudes.index(min_latitude)]
-max_latitude_coord = coordinates[latitudes.index(max_latitude)]
-
-print(f"Minimum Longitude (x): {min_longitude} found at {min_longitude_coord}")
-print(f"Maximum Longitude (x): {max_longitude} found at {max_longitude_coord}")
-print(f"Minimum Latitude (y): {min_latitude} found at {min_latitude_coord}")
-print(f"Maximum Latitude (y): {max_latitude} found at {max_latitude_coord}")
-
-for i in range(len(created)):
-    print(i)
