94104dcd59b1e1ecfefe130c74cf21e74d807d99
Bejap <benjaminsch08@gmail.com>
2024-10-31 15:22:24 +0100

    Working on Whist

--- 70_projects/whist/SeperateClass/Card.py
+++ 70_projects/whist/SeperateClass/Card.py
@@ -5,3 +5,14 @@ class Card:
 
     def __str__(self):
         return f'{self.suit} of {self.rank}'
+
+    def _get_short_rank(self):
+        if self.rank == '10':
+            return '10'
+        return self.rank[0].upper()
+
+    def _get_short_suit(self):
+        return self.suit[0].upper()
+
+    def get_short_name(self):
+        return f"{self._get_short_rank()}o{self._get_short_suit()}"
--- 70_projects/whist/SeperateClass/Player.py
+++ 70_projects/whist/SeperateClass/Player.py
@@ -1,40 +1,32 @@
 from Card import Card
 
 class Player:
-    def __init__(self, name: str):
+
+    def __init__(self, name: str, is_human: bool):
         self.name = name
-        self.hand: list[Card] = []
-        self.tricks_won: int = 0
+        self.hand = []
+        self.tricks_won = 0
+        self.is_humna = is_human
+        self.cards_seen = set()
 
-    def play_card(self, playable_cards: list[Card], trump_suit: str, lead_suit: str=None, played_cards=None):
+    def play_card(self, playable_cards: list[Card], trump_suit: str, lead_suit=None, played_cards=None):
+        playable_cards.sort()
 
-        if self.is_human:
+        while True:
+            print(f"\nYour hand: {[card.get_short_name() for card in sorted(self.hand)]}")
+            print(f"Playable cards: {[card.get_short_name() for card in playable_cards]}")
+            if lead_suit:
+                print(f"Lead suit: {lead_suit}")
 
-            playable_cards.sort()
-            while True:
-                print(f"\nplayable cards: {playable_cards}")
-                if lead_suit:
-                    print(f'Lead Suit: {lead_suit}')
-
-                try:
-                    card_idx = int(input(f'Enter the number (1-{len(playable_cards)}) of card to play: ')) - 1
-                    if 0 <= card_idx < len(playable_cards):
-                        card = playable_cards[card_idx]
-                        self.hand.remove(card)
-                        print(f'You played: {card}')
-                        return card
-                    else:
-                        print('Invalid card number')
-                except ValueError:
-                    print('Invalid card number')
-        else:
-            best_card = None
-            best_score = -1
-            played_cards = played_cards or []
-
-            for card in playable_cards:
-                score = self.evaluate_card_strength(card, trump_suit, lead_suit)
-
-                if not lead_suit: # if player starts round assign scores to the cards.
-
-
+            try:
+                card_idx = int(input(f"Enter the number (1-{len(playable_cards)}) of the card you want to play: ")) - 1
+                if 0 <= card_idx < len(playable_cards):
+                    card = playable_cards[card_idx]
+                    self.hand.remove(card)  # Remove card from hand
+                    print(f"You played: {card.get_short_name()}")
+                    time.sleep(1)
+                    return card
+                else:
+                    print("Invalid card number. Please try again.")
+            except ValueError:
+                print("Please enter a valid number.")
