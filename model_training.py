460e2c4b6d68602977fc6135e88f134c6c33aae8
bes <bes@kruso.dk>
2025-11-20 13:27:18 +0100

    correct display of q_value

--- model_training.py
+++ model_training.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
                                 # Get the card index with highest Q-value
                                 action, q_value = max(valid_q_values, key=lambda x: x[1])
                                 decision_type = 'agent'
-                                certainty = float(q_value)
+                                certainty = float(q_value[0])
                             else:
                                 action = np.random.choice(valid_actions) if valid_actions else 0
                                 decision_type = 'random'
