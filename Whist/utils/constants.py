c7f67cbdbeab5d4d2951cb2b90a07703a39a74e7
bes <bes@kruso.dk>
2025-11-26 14:09:52 +0100

    message

--- Whist/utils/constants.py
+++ Whist/utils/constants.py
@@ -82,7 +82,7 @@ MEMORY_FRACTION = 0.35  # GPU memory fraction to use
 DEFAULT_GAMMA = 0.99
 
 # Model save threshold: only save model if average reward is above this value
-MODEL_SAVE_REWARD_THRESHOLD = -5.5
+MODEL_SAVE_REWARD_THRESHOLD = -3.0
 
 # Model save check frequency: check every N games after minimum games played
 MODEL_SAVE_CHECK_EVERY = 25  # Check every 25 games
